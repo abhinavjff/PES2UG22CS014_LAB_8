1fff352f3c4806454e19682df7133a3eff9025b9
Abhinav Kalyan Talluri <113676352+abhinavjff@users.noreply.github.com>
2025-03-11 16:03:40 +0530

    Update hello.cpp more

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout<<"i have added the webhook"";
+  cout<<"i have added the webhook";
   return 0;
 }
